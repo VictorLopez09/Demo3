161f67c30e00261a5abaf27fd489e25916bfb604
VictorLopez09 <106134067+VictorLopez09@users.noreply.github.com>
2023-03-21 17:38:44 -0600

    Add files via upload

---
+++ ejecutar.php
@@ -0,0 +1,9 @@
+<?php
+    $nombre=$_POST['nombre'];
+    $edad=$_POST['edad'];
+    $sueldo=$_POST['sueldo'];
+
+    echo "Tu nombre es".$nombre; 
+    echo "Tu edad es: ".$edad;
+    echo "Tu sueldo: ".$sueldo;
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo 3</title>
+</head>
+<body>
+    <form action="ejecutar.php" method="post">
+        <label for="">Nombre</label>
+        <input type="text" name="nombre">
+        <label for="">Edad</label>
+        <input type="text" name="edad">
+        <label for="">Sueldo</label>
+        <input type="number" name="sueldo">
+        <input type="submit" value="Enviar">
+    </form>
+
+</body>
+</html>
